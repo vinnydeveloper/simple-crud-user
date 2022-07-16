ff158178a60f7424674a488e350935217b3596fc
Vinicius Oliveira <vinicius.developer@outlook.com>
2022-07-16 11:40:41 -0300

    rota home

--- src/infra/BaseRoutes/index.ts
+++ src/infra/BaseRoutes/index.ts
@@ -1,10 +1,13 @@
-import { Router } from "express";
+import { Router, Response, Request } from "express";
 
 import userRoutes from "../../modules/User/routes";
 import authRoutes from "../../modules/Auth/routes";
 
 const routes = Router();
 
+routes.get("/", (req: Request, res: Response) => {
+  return res.json("Api rodando corretamente");
+});
 routes.use(userRoutes);
 routes.use(authRoutes);
 
